0952c4c8bb29ecda1a6b0d02dd96bcbea445b9f0
visualstudio <visualstudio@122-PC31>
2019-04-10 10:42:37 -0400

    Section 3

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
 {
     class Program
     {
-        ArrayList             Beowulf;
-        static      void              Main  (string[] args)
+        ArrayList Beowulf;
+        static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
@@ -25,30 +25,36 @@ namespace ConsoleApp1
             using (StreamReader file = new StreamReader(@"U:\Users\732125\New folder/beowulf.txt"))
             {
                 int counter = 0;
-                string ln;
 
-                while ((ln = file.ReadLine()) != null)
+                int number = 0;
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+
                 {
-                    Console.WriteLine(ln);
-                    Beowulf.Add(ln);
+
+                    if (line.Contains("Sea") && line.Contains("Fare") || line.Contains("sea") && line.Contains("fare"))
+
+                    {
+
+                        int x = counter - 1;
+
+                        number++;
+
+                    }
+
+                    counter++;
+
                 }
+
+                Console.WriteLine($"The number of lines that contains *Sea* and *Fare* are {number}");
+
+
+
                 file.Close();
-                Console.WriteLine($"File has {counter} lines.");
             }
         }
-        public int FindNumberOfBlankSpaces(string line)
-        {
-            int countletters = 0;
-            int countSpaces = 0;
-
-            foreach (char c in line)
-            {
-                if (char.IsLetter(c)) { countletters++; }
-                if (char.IsWhiteSpace(c)) { countSpaces++; }
-            }
-            return countSpaces;
-        }
-
     }
 }
 
